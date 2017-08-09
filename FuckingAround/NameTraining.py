f0a44520b92a8ed49b56676b2118c1eabcbb3859
Kyle <kyodonne@gmail.com>
2017-08-08 20:59:45 -0500

    Simple classes

---
+++ FuckingAround/NameTraining.py
@@ -0,0 +1,17 @@
+class NameTraining(object):
+	def __init__(self):
+		print ("NameTraining Initialized")
+
+	def train(self):
+		#We're going to let user know that they're a faggot
+		#Becase on the internet everyone is automatically a faggot regardless of sexual orientation
+		#Basically mixing strings with print function
+		name = input("What is your name? ")
+		
+		if name.upper() == "FAGGOT" :
+			print ("Good boy!")
+		else:
+			print("You think your name is " + name)
+			print("Really though, your name is Faggot, and you're a cunt.")
+			print("If you are offended thus far, you're a salty cunt.")
+			print("The programmer is an ass, but that doesn't make you any less of a cunt.")
